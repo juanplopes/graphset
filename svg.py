b1d0503e1a8b59306c233457a675a8c7e6896c20
Juan Lopes <me@juanlopes.net>
2016-07-26 13:21:10 -0300

    full

--- svg.py
+++ svg.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import json, subprocess
+import json, subprocess, sys
 
 def to_svg(G):
     content = ['graph{bgcolor="transparent";']
@@ -9,14 +9,27 @@ def to_svg(G):
             if x&(1<<j):
                 content.append('{}--{};'.format(i, j))
     content.append('}')
-    print(''.join(content))
     return subprocess.check_output(['neato', '-Tsvg'], input=bytes(''.join(content), 'utf-8'))
 
-with open('data.js') as f:
-    s = f.read();
-    s = s[s.find('['):s.rfind(';')];
-    s = json.loads(s)
+def normalize(graph):
+    graph['n'] = len(graph['G'])
+    graph['b'] = max(x.bit_length() for x in graph['S'])
 
-    for i, graph in enumerate(s):
-        with open('thumbs/{}.svg'.format(i), 'wb') as g:
+
+data = []
+with open(sys.argv[1]) as f:
+    for i, line in enumerate(f):
+        print('Graph #{}'.format(i))
+
+        s = line[line.find('{'):line.rfind('}')+1];
+        graph = json.loads(s)
+       
+        normalize(graph)
+        with open('html/thumbs/{}.svg'.format(i), 'wb') as g:
             g.write(to_svg(graph['G']))
+            
+        data.append(graph)
+        
+with open('html/data.js'.format(i), 'w') as g:
+    g.write('var data = {};'.format(json.dumps(data)))
+
