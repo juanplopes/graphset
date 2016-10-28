60e3fcb4df1ac1b81c1f31bd6a8de69ab9fae9a1
Juan Lopes <me@juanlopes.net>
2016-10-28 08:09:09 -0200

    test2

--- test2.cpp
+++ test2.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <cstring>
 using namespace std;
 
+bool GG[1000][1000];
 vector<int> G[1000];
 int T[1000][1000];
 
 int XX = 1;
-bool V[1000];
-
 void setNext(int k, int from, int to) {
     T[to][0] = 0;
     for(int i=0; i<k; i++) {
@@ -20,6 +20,13 @@ void setNext(int k, int from, int to) {
                 T[to][start+j] = T[to][j];
         }
     }
+    
+/*    cout << "////" << to << " " ;
+    for(int i=0; i<(1<<k); i++) {
+        cout << " " << T[to][i];
+    }
+    cout << endl;*/
+    
     for(int i=1<<((k+1)-1); i>=0; i--) {
         int value = T[to][i];
         T[to][i*2 + value] = T[from][i*2 + value];
@@ -45,27 +52,48 @@ void dfs(int k, int level, int u, int v) {
     }
 }
 
+bool test(int k, int u, int v) {
+    int eq = 0, total = 0;
+    for(int i=0; i<(1<<(k+1)); i++) {
+        if (!T[u][i] && !T[v][i]) continue;
+        if (T[u][i] == T[v][i])      { eq++; total++; }
+        else if (T[u][i] && T[v][i]) { total += 2; }
+        else if (T[u][i] || T[v][i]) { total += 1; }
+    }
+//    cout << "       " << u << " " << v << " " << GG[u][v] << " " << eq << " " << total << endl;
+    return GG[u][v] ? eq*2>=total : eq*3<=total; 
+}
+
 int main() {
     int N, M;
     while(cin >> N >> M) {
+        memset(GG, 0, sizeof GG);
         for(int i=0; i<M; i++) {
             int a, b; cin >> a >> b;
             G[a].push_back(b);
             G[b].push_back(a);        
+            GG[a][b] = GG[b][a] = true;
         }
         
-        int k = ceil(log(N)/log(2))-1;
+        int k = ceil(log(N)/log(2));
+//        cout << k << " " << (1<<(k+1)) << endl;
         dfs(k, 0, -1, 0);
         
         for(int i=0; i<N; i++) {
-            cout << i << ":";
-
+            for(int j=0; j<N; j++) {
+                if (i!=j && !test(k, i, j)) cout << "  ERROR" << i << " " << j << endl;
+            }
+        
+        }
+        
+        for(int i=0; i<N; i++) {
+            G[i].clear();
+  /*          cout << i << ":";
             for(int j=0; j<(1<<(k+1)); j++) {
                 if (T[i][j])
                     cout << " " << T[i][j];
             }
-            
-            cout << endl;
+            cout << endl;*/
         }
     }
 }
